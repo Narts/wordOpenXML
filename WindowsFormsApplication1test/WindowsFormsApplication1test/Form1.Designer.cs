cd4f63975f1dafc2b94a7a95e686d8305878fad7
Narts <taoransheng@googlemail.com>
2013-06-03 17:08:31 +0200

    ini
    initialcommit

---
+++ WindowsFormsApplication1test/WindowsFormsApplication1test/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApplication1test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BuildButton = new System.Windows.Forms.Button();
+            this.SaveFile = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.CreateFolderButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IEButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(18, 446);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(108, 23);
+            this.InsertButton.TabIndex = 0;
+            this.InsertButton.Text = "Insert Content";
+            this.InsertButton.UseVisualStyleBackColor = true;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(562, 148);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // BuildButton
+            // 
+            this.BuildButton.Location = new System.Drawing.Point(136, 446);
+            this.BuildButton.Name = "BuildButton";
+            this.BuildButton.Size = new System.Drawing.Size(108, 23);
+            this.BuildButton.TabIndex = 2;
+            this.BuildButton.Text = "Build Summary";
+            this.BuildButton.UseVisualStyleBackColor = true;
+            this.BuildButton.Click += new System.EventHandler(this.BuildButton_Click);
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.Location = new System.Drawing.Point(352, 446);
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Size = new System.Drawing.Size(108, 23);
+            this.SaveFile.TabIndex = 3;
+            this.SaveFile.Text = "Save File";
+            this.SaveFile.UseVisualStyleBackColor = true;
+            this.SaveFile.Click += new System.EventHandler(this.SaveFileButton_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // CreateFolderButton
+            // 
+            this.CreateFolderButton.Location = new System.Drawing.Point(466, 446);
+            this.CreateFolderButton.Name = "CreateFolderButton";
+            this.CreateFolderButton.Size = new System.Drawing.Size(108, 23);
+            this.CreateFolderButton.TabIndex = 4;
+            this.CreateFolderButton.Text = "Create Folder";
+            this.CreateFolderButton.UseVisualStyleBackColor = true;
+            this.CreateFolderButton.Click += new System.EventHandler(this.CreateFolderButton_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 234);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(562, 166);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Content";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Commentar";
+            // 
+            // IEButton
+            // 
+            this.IEButton.Location = new System.Drawing.Point(270, 422);
+            this.IEButton.Name = "IEButton";
+            this.IEButton.Size = new System.Drawing.Size(75, 23);
+            this.IEButton.TabIndex = 8;
+            this.IEButton.Text = "IE Button";
+            this.IEButton.UseVisualStyleBackColor = true;
+            this.IEButton.Click += new System.EventHandler(this.IEButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 481);
+            this.Controls.Add(this.IEButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.CreateFolderButton);
+            this.Controls.Add(this.SaveFile);
+            this.Controls.Add(this.BuildButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.InsertButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Summary Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InsertButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BuildButton;
+        private System.Windows.Forms.Button SaveFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button CreateFolderButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button IEButton;
+    }
+}
+
---
+++ WindowsFormsApplication1test/WindowsFormsApplication1test/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApplication1test
+{
+    public partial class Form1 : Form
+    {
+        String saved_document;
+        String saved_commentar;
+        List<String> saved_doc_list = new List<String>();
+        List<String> saved_com_list = new List<String>();
+        String saved_path;
+        String opened_folder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+            String doc = saved_document;
+            String com = saved_commentar;
+            saved_doc_list.Add(doc);
+            saved_com_list.Add(com);
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string str = "";
+            foreach (string line in richTextBox1.Lines)
+                str += line;
+            get_document(str);
+        }
+
+        private void BuildButton_Click(object sender, EventArgs e)
+        {
+            String show_doc = "";
+            String show_com = "";
+            String show_together = "";
+
+            for (int i = 0; i < saved_doc_list.Count(); i++)
+            {
+                show_doc = saved_doc_list[i];
+                show_com = saved_com_list[i];
+                show_together = "\n"+ show_doc + "\n" + show_com;
+                MessageBox.Show(show_together);
+            }
+            saved_doc_list.Clear();
+            saved_com_list.Clear();
+        }
+
+        private void get_document(String str)
+        {
+            saved_document = str;
+        }
+
+        private void SaveFileButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.Title = "Save text Files";
+            saveFileDialog1.CheckFileExists = true;
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                saved_path = saveFileDialog1.FileName;
+                MessageBox.Show(saved_path);
+
+            } 
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void CreateFolderButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            // Show the FolderBrowserDialog. 
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                opened_folder = folderDlg.SelectedPath;
+                MessageBox.Show(opened_folder);
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            } 
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            string str = "";
+            foreach (string line in richTextBox2.Lines)
+                str += line;
+            get_commentar(str);
+        }
+
+        private void get_commentar(String str)
+        {
+            saved_commentar = str;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IEButton_Click(object sender, EventArgs e)
+        {
+            Process[] current_Process = Process.GetProcessesByName("NOTEPAD");
+            foreach(var item in current_Process)
+            {
+                String full_path = item.Modules[0].FileName;
+                String full_path_test = Path.GetFullPath(item.ToString());
+                //String str = System.IO.Path.GetDirectoryName(item.ProcessName);
+                MessageBox.Show(full_path_test);
+            }
+        }
+    }
+}
